bff031433eb3c14f02e8c6f517e9c79e73798d65
camilleechip <camilleechip@gmail.com>
2025-04-30 12:45:22 -0600

    image update

--- mission/scripts/mission.js
+++ mission/scripts/mission.js
@@ -1,7 +1,16 @@
-const themeSelector = document.getElementById('theme-selector');
-
-themeSelector.addEventListener('change', function() {
+const themeSelector = document.querySelector('#theme-selector');
+function changeTheme() {
     const selectedTheme = themeSelector.value;
+    document.body.classList.remove('light', 'dark')
+    document.body.classList.add(selectedTheme); 
+    
+    const footerImage = document.getElementById('footer-img');
+    if (selectedTheme === 'dark') {
+        footerImage.src = 'images/byui-logo_dark.png';
+    } else {
+        footerImage.src = 'images/byu-logo.webp';
+    }
+}
 
-    document.body.className = selectedTheme;
-});
+themeSelector.addEventListener('change', changeTheme)
+changeTheme();

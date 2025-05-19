8ff51755267a9ebfae0ddf98e548c42d81c3c0a3
camilleechip <camilleechip@gmail.com>
2025-05-19 11:25:11 -0600

    modal/dialog

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -2,3 +2,25 @@ function toggleMenu() {
   const navLinks = document.getElementById("nav-links");
   navLinks.classList.toggle("show");
 }
+ 
+function showModal(imageSRC, imgAlt) {
+  const dialog = document.createElement('dialog');
+  dialog.innerHTML = `
+    <img src="${imageSRC}" alt="${imgAlt}">
+    <button class="close-viewer">X</button>
+    `;
+    document.body.append(dialog);
+    dialog.showModal();
+
+    dialog.querySelector('.close-viewer').addEventListener('click', () => {
+      dialog.close();
+      dialog.remove();
+    });
+  }
+
+  document.querySelectorAll('.custom-img').forEach(img => {
+    img.addEventListener('click', () => {
+      showModal(img.src, img.alt);
+    });
+  });
+  
